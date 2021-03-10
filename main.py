0d200ad21e9b52aa399b9830ab20353ed92f98a2
repl.it user <replituser@example.com>
2021-03-10 07:29:04 +0000

    First activity with logical operator OR

---
+++ main.py
@@ -0,0 +1,10 @@
+#Retrive form from user
+print("What shape do I have?")
+shape=input()
+
+#Identify the shape
+if((shape=="square") or (shape=="rectangle") or (shape =="rhombus")):
+  print("This shape is a parallelogram")
+else:
+  print("This shape is not a parallelogram.")
+  
